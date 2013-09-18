33d7d2105e7a2f600d6d162742619d517f3466d5
Erik Rumppe <erumppe@gmail.com>
2013-09-18 11:49:47 -0700

    Installed fields and platform core plugins.  Refreshed dependencies.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,7 +32,9 @@ grails.project.dependency.resolution = {
 
         compile ':cache:1.0.1'
    		compile ':cloud-support:1.0.8'
+        compile ":fields:1.3"
    		compile ':heroku:1.0.1'
+        compile ":platform-core:1.0.RC5"
    		compile ":twitter-bootstrap:3.0.0"
    		
         runtime ":hibernate:$grailsVersion"
